add7c594044f98ac817f58cbe81eb88e44413771
liyanj <gnoluna@gmail.com>
2023-12-31 12:10:08 +0100

    Add cargo template

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # leetcode-cookbook-rust
 
 - [x] [Two sum](https://leetcode.com/problems/two-sum/description/)
+- [x] [Add Two Numbers](https://leetcode.com/problems/add-two-numbers/)
+
+
+## How to use template
+
+Use [cargo-generate](https://github.com/cargo-generate/cargo-generate)
+
+```
+cargo generate --init --path template
+```
---
+++ template/placeholders/src/leetcode/{{problem}}.rs
@@ -0,0 +1,19 @@
+
+
+pub struct Solution;
+
+impl Solution {
+    pub fn {{problem}}(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        [0,0]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::leetcode::{{problem}}::Solution;
+
+    #[test]
+    fn case1() {
+        assert_eq!(Solution::{{problem}}(nums, target), vec![0, 1]);
+    }
+}
